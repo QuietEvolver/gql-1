7da09a1e443916f1bf5c0e1033b32db2e9ec2d00
VW <vera.weikel@gmail.com>
2023-09-10 17:59:39 -0700

    Add type Track{}: server/src/schema.js

--- server/src/schema.js
+++ server/src/schema.js
@@ -3,4 +3,16 @@ const gql = require("graphql-tag");
 const typeDefs = gql`
     # Schema definitions go here
 `;
+
+"A track is a group of Modules that teaches about a specific topic"
+type Track {
+    # Fields go here
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+}
+
 module.exports = typeDefs;
