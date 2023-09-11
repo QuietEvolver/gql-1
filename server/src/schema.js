36789e49c30fed2c83fdf56038afc980047787e6
VW <vera.weikel@gmail.com>
2023-09-10 18:21:37 -0700

    Query type fields define which data clients can query from schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,31 +2,37 @@ const gql = require("graphql-tag");
 
 const typeDefs = gql`
     # Schema definitions go here
+        
+    type Query {
+        # Fields go here 
+        "Get tracks array for homepage grid"
+        tracksForHome: [Track!]!
+    }
+
+    "A track is a group of Modules that teaches about a specific topic"
+    type Track {
+        # Fields go here
+        id: ID!
+        "Thr track's title"
+        title: String!
+        "The track's main author"
+        author: Author!
+        "Thr track's main illustration to display in track card or track page detail"
+        thumbnail: String
+        "The track's approximate length to complete, in minutes"
+        length: Int
+        "The number of modules this track contains"
+        modulesCount: Int
+    }
+
+    "Author of the complete Track or a Module"
+    type Author {
+        id: ID!
+        "Author's first and last name"
+        name: String!
+        "Author's profile picture url"
+        photo: String
+    }
 `;
-
-"A track is a group of Modules that teaches about a specific topic"
-type Track {
-    # Fields go here
-    id: ID!
-    title: String!
-    author: Author!
-    thumbnail: String
-    length: Int
-    modulesCount: Int
-}
-
-"Author of the complete Track or a Module"
-type Author {
-    id: ID!
-    name: String!
-    photo: String
-}
-
-type Query {
-    # Fields go here 
-    "Get tracks array for homepage grid"
-    tracksForHome: [Track!]!
-}
-
 
 module.exports = typeDefs;
