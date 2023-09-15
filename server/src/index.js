474140c1bee403feaa999336a3103367aae29bdf
VW <vera.weikel@gmail.com>
2023-09-15 07:47:15 -0700

    Imports on server/src/index.js file.

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,6 @@
+const { ApolloServer } = require("@apollo/server"); // The Apollo Server library helps us implement this server quickly, painlessly, and in a production-ready way.
+const { startStandaloneServer } = require("@apollo/server/standalone"); // import the startStandaloneServer function from the @apollo/server/standalone package.
+const typeDefs = require("./schema"); // typeDefs from our schema.js file
+
+
 //TODO
