e5b7a8b182e240d83a747f547978d67dddc798f3
VW <vera.weikel@gmail.com>
2023-09-15 07:52:08 -0700

    Add async fxn to create server instance and pass it to types defines in schema.

--- server/src/index.js
+++ server/src/index.js
@@ -2,5 +2,8 @@ const { ApolloServer } = require("@apollo/server"); // The Apollo Server library
 const { startStandaloneServer } = require("@apollo/server/standalone"); // import the startStandaloneServer function from the @apollo/server/standalone package.
 const typeDefs = require("./schema"); // typeDefs from our schema.js file
 
-
+///  set up an async function called startApolloServer. Inside, we'll create an instance of the ApolloServer class and pass it our typeDefs in its options object
+async function startApolloServer(){
+    const server = new ApolloServer({typeDefs});
+}
 //TODO
