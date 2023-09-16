5f5bd00f3a2f75c7c91713c38b32b61cf56ae813
VW <vera.weikel@gmail.com>
2023-09-16 09:43:56 -0700

    Add notes.

--- server/src/index.js
+++ server/src/index.js
@@ -5,5 +5,8 @@ const typeDefs = require("./schema"); // typeDefs from our schema.js file
 ///  set up an async function called startApolloServer. Inside, we'll create an instance of the ApolloServer class and pass it our typeDefs in its options object
 async function startApolloServer(){
     const server = new ApolloServer({typeDefs});
+    // Note: We're using shorthand property notation with implied keys, because we've named our constant with the matching key (typeDefs).
+    startStandaloneServer(server);// initialize server
+//The startStandaloneServer function returns a Promise, so we'll await the results of that call, and pull out the url property from the result.
 }
 //TODO
