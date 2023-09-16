e451cf2ee6a4a7cf52a40ed2da2d46f95250cdcd
VW <vera.weikel@gmail.com>
2023-09-16 11:27:07 -0700

    Add pkgs; tweak the ApolloServer initialization tba.

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require("@apollo/server"); // The Apollo Server library helps us implement this server quickly, painlessly, and in a production-ready way.
 const { startStandaloneServer } = require("@apollo/server/standalone"); // import the startStandaloneServer function from the @apollo/server/standalone package.
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
 const typeDefs = require("./schema"); // typeDefs from our schema.js file
 
 ///  set up an async function called startApolloServer. Inside, we'll create an instance of the ApolloServer class and pass it our typeDefs in its options object
