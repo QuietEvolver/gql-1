98deddbe499e52b2f44c4fe8e166d10ab8d586ae
VW <vera.weikel@gmail.com>
2023-09-16 17:26:32 -0700

    Add mocks object.

--- server/src/index.js
+++ server/src/index.js
@@ -13,3 +13,22 @@ const server = new ApolloServer({
 });
 // actually call the function at the bottom of the function declaration
 ApolloServer();
+
+const mock = {
+    Track: () => 
+    ({
+        id: () => "track_01", 
+        title: () => "Astro Kitty, Space Explorer", 
+        author: () => 
+        {
+            return {
+                name: "Grumpy Cat", 
+                photo: " https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mtcf3c.jpg",
+            };
+        },
+        thumbnail: () => 
+        "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+        length: () => 1210, 
+        modulesCount: () => 6, 
+    }),
+};
